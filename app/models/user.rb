938bee7750085679bb41ae74037751b16b20db10
Terence Dignon <terry.p.dignon@gmail.com>
2017-07-22 23:22:54 -0400

    add self.model_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   self.inheritance_column = 'user_type'
+
+  def self.model_name
+    ActiveModel::Name.new(self, nil, 'User')
+  end
+
+  def to_partial_path
+    'users/user'
+  end
 end
